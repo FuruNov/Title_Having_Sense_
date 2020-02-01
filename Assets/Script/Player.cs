fe0b40a85ffa9ab2ae85d4a459eb1c35d60d72e0
FuruNov <furuhashitakanobu0413@gmail.com>
2020-02-01 20:07:34 +0900

    Commit

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
 {
     //基礎ステータス
     [SerializeField] float move_speed;
+    [SerializeField] float HP;
     [SerializeField] float attack;
     [SerializeField] float defense;
 
@@ -18,12 +19,22 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
     }
 
     void Move()
     {
+        //移動方向の指定
+        var direction_x = 0f; var direction_y = 0f;
+        if (Input.GetKey(KeyCode.UpArrow)) { direction_y = move_speed; }
+        else if (Input.GetKey(KeyCode.DownArrow)) { direction_y = -(move_speed); }
+        else if (Input.GetKey(KeyCode.LeftArrow)) { direction_x = -(move_speed); }
+        else if (Input.GetKey(KeyCode.RightArrow)) { direction_x = move_speed; }
+        else { direction_x = 0f; direction_y = 0f; }        
 
+        //移動
+        var velocity = new Vector2(direction_x,direction_y);
+        transform.Translate(velocity);
     }
 
     void Find()
@@ -34,5 +45,10 @@ public class Player : MonoBehaviour
     void Attack()
     {
 
+    }
+
+    void Dead()
+    {
+        if(HP == 0) { }
     }
 }
